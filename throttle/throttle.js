3ea0cf5717f6cd1fb514df5bc7dd5cfb59d1c8ab
pooja yadav <96077451+pooja171195@users.noreply.github.com>
2022-04-28 17:40:36 +0530

    updated

--- throttle/throttle.js
+++ throttle/throttle.js
@@ -1,15 +1,19 @@
 const throttler = (fn, delay) => {
     let lastCalled = 0;
+   
     return (...args) => {
       let now = new Date().getTime();
       if(now - lastCalled < delay) {
-        return;
+          count++
+        return ;
       }
       lastCalled = now;
       return fn(...args);
     }
   }
-  
+
+
+
   const printMessage = () => {
     //console.log("");
     let ele = document.getElementById('container');
@@ -20,3 +24,20 @@ const throttler = (fn, delay) => {
 };
   
   const sayHello = throttler(printMessage,2000);
+
+
+  function throttle( fn, wait ){
+    let lastCall = 0;
+      return function(){
+      if( Date.now() - lastCall > wait  ){
+        lastCall = Date.now()
+        fn()
+      }
+    }
+  }
+  
+  var counter = 0
+  
+  var count = () => document.querySelector("#count").textContent = ++counter
+  
+  window.addEventListener('scroll', throttle( count, 1000 ) )
